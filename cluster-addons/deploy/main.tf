057f707352cff225337dae808b51c9a55cdf17cd
AymenSegni <segniaymen1@gmail.com>
2025-12-22 17:27:38 +0100

    Fix namespace to use bool

--- cluster-addons/deploy/main.tf
+++ cluster-addons/deploy/main.tf
@@ -13,7 +13,7 @@ module "cluster_addons" {
   timeout                   = 300
   wait                      = true
   name                      = "addons"
-  create_namespace          = true
+  create_namespace          = false
   service_account_name      = var.service_account_name
   service_account_namespace = var.namespace
   kubernetes_namespace      = var.namespace
