7c2f707b29cfa3f6b10a1cd712f636ab74396fa0
AymenSegni <segniaymen1@gmail.com>
2025-12-22 16:50:09 +0100

    use helm release module directly

--- btcd-core/deploy/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "release_name" {
-  description = "Helm release name"
-  value       = module.bitcoind.release_name
-}
-
-output "namespace" {
-  description = "Kubernetes namespace"
-  value       = var.namespace
-}
